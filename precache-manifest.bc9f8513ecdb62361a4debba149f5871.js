b5b923881e3fdbbf599a27279c2d9a8427e38838
Keith Charlene Esmena <kc@dhcp-10-22-41-235.wlan.ntnu.no>
2020-09-10 18:00:39 +0200

    Updates

---
+++ precache-manifest.bc9f8513ecdb62361a4debba149f5871.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a63036d94b726811ec993d5281dd135",
+    "url": "/infoskjerm/index.html"
+  },
+  {
+    "revision": "eac9d52e8c5cdfba0af1",
+    "url": "/infoskjerm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b17a53f07ecfc66ce261",
+    "url": "/infoskjerm/static/js/2.ea0444ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infoskjerm/static/js/2.ea0444ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eac9d52e8c5cdfba0af1",
+    "url": "/infoskjerm/static/js/main.a4b88680.chunk.js"
+  },
+  {
+    "revision": "6fa4fe89daa3d5d07bae",
+    "url": "/infoskjerm/static/js/runtime-main.d6c9a5ab.js"
+  }
+]);
